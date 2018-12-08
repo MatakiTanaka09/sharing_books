0929b2035c3e05bde3923c1f4f8e2acaf9f94575
Mataki Tanaka <mataki.tanaka.works@gmail.com>
2018-12-08 16:08:26 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from '@/store/store'
+import { sync } from 'vuex-router-sync'
+
+// You can choose css flamework which you uses buefy(bulma) or Bootstrap
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+sync(store, router)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+}).$mount('#app')
